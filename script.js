ad9351c54cc6ef78dbc297cbdf473b7dbed56bcc
RealFTWhiskey <tadeuzrydberg@outlook.com>
2025-01-03 13:31:37 +0100

    Update

--- script.js
+++ script.js
@@ -14,46 +14,46 @@ document.getElementById('exampel').addEventListener('click', function () {
   window.location.href = 'classroomex.html';
 });
 
-const monthYearElement = document.getElementById('monthYear')
-const datesElement = document.getElementById('dates')
-const prevBtn = document.getElementById('prevBtn')
-const nextBtn = document.getElementById('nextBtn')
+const monthYearElement = document.getElementById('monthYear');
+const datesElement = document.getElementById('dates');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
 
-let currentDate =new Date();
+let currentDate = new Date();
 
 const updateCalender = () => {
   const currentYear = currentDate.getFullYear();
   const currentMonth = currentDate.getMonth();
 
   const firstday = new Date(currentYear, currentMonth, 0);
-  const lastDay = new Date(currentYear, currentMonth +1, 0);
+  const lastDay = new Date(currentYear, currentMonth + 1, 0);
   const totalDays = lastDay.getDate();
   const firstDayIndex = firstday.getDate();
   const lastDayIndex = lastDay.getDay();
 
   const monthYearString = currentDate.toLocaleString
-  ('default', {month: 'long', year: 'numeric'});
-  monthYearElement.textContent = monthYearString
+    ('default', { month: 'long', year: 'numeric' });
+  monthYearElement.textContent = monthYearString;
 
   let datesHTML = '';
 
-  for(let i = firstDayIndex; i > 0; i--) {
+  for (let i = firstDayIndex; i > 0; i--) {
     const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
     datesHTML += `<div class="date inactive">${prevDate.getDate()}</div>`;
   }
 
-  for(let i = 1; i <= totalDays; i++) {
+  for (let i = 1; i <= totalDays; i++) {
     const date = new Date(currentYear, currentMonth, i);
     const activeClass = new date.toDateString() == new Date().toDateString() ? 'active' : '';
     datesHTML += `<div class="date ${activeClass}">${i}</div>`;
   }
 
-  for(let i = 1; i <= 7 - lastDayIndex; i++) {
+  for (let i = 1; i <= 7 - lastDayIndex; i++) {
     const nextDate = new Date(currentYear, currentMonth, + 1, i);
     datesHTML += `<div class="date inactive">${nextDate.getDate()}</div>`;
   }
 
-  datesElement.innerHTML =datesHTML
+  datesElement.innerHTML = datesHTML;
 }
 
 prevBtn.addEventListener('click', () => {
